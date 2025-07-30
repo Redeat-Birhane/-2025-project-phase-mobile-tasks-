07dbe405431bf5b241659b24220a2b4200c30b06
Redeat Birhane <149287082+Redeat-Birhane@users.noreply.github.com>
2025-07-30 11:44:12 -1100

    Add data sources and implement ProductRepositoryImpl; update product repository interface

--- mobile/redeat/lib/domain/repositories/product_repository.dart
+++ mobile/redeat/lib/domain/repositories/product_repository.dart
@@ -1,9 +1,9 @@
 import '../entities/product.dart';
 
 abstract class ProductRepository {
+  Future<List<Product>> getAllProducts();
+  Future<Product?> getProductById(String id);
   Future<void> insertProduct(Product product);
   Future<void> updateProduct(Product product);
   Future<void> deleteProduct(String id);
-  Future<Product?> getProduct(String id);
-  Future<List<Product>> getAllProducts();
 }
