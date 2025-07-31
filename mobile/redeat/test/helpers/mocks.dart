98483ad144f90431c9872600e5c4ea1e6b19cb87
Redeat Birhane <149287082+Redeat-Birhane@users.noreply.github.com>
2025-07-31 08:10:44 -1100

    Implement ProductRepositoryImpl tests with mock dependencies

--- mobile/redeat/test/helpers/mocks.dart
+++ mobile/redeat/test/helpers/mocks.dart
@@ -1,7 +1,7 @@
 import 'package:mockito/mockito.dart';
 import 'package:my_first_app/data/datasources/product_remote_data_source.dart';
 import 'package:my_first_app/data/datasources/product_local_data_source.dart';
-import 'package:my_first_app/core/network/network_info.dart';
+
 
 
 class MockProductRemoteDataSource extends Mock implements ProductRemoteDataSource {}
@@ -9,4 +9,4 @@ class MockProductRemoteDataSource extends Mock implements ProductRemoteDataSourc
 class MockProductLocalDataSource extends Mock implements ProductLocalDataSource {}
 
 
-class MockNetworkInfo extends Mock implements NetworkInfo {}
+
