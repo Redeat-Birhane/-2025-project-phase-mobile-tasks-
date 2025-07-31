ccd07c8ba8a739695d28d46e280134b7e01ba2d7
Redeat Birhane <149287082+Redeat-Birhane@users.noreply.github.com>
2025-07-31 10:43:41 -1100

    Fix repository tests and resolve conflicts without using any

--- mobile/redeat/test/features/product/data/datasources/product_remote_datasource_impl_test.dart
+++ mobile/redeat/test/features/product/data/datasources/product_remote_datasource_impl_test.dart
@@ -1,53 +1,87 @@
-import 'dart:convert';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:http/http.dart' as http;
 import 'package:mockito/mockito.dart';
 import 'package:my_first_app/core/features/product/data/models/product_model.dart';
-import 'package:my_first_app/data/datasources/product_remote_datasource_impl.dart';
+import 'package:my_first_app/data/datasources/product_local_data_source.dart';
+import 'package:my_first_app/data/datasources/product_remote_data_source.dart';
+import 'package:my_first_app/data/repositories/product_repository_impl.dart';
+import 'package:my_first_app/domain/entities/product.dart';
+import 'package:my_first_app/core/network/network_info.dart';
 
-class MockHttpClient extends Mock implements http.Client {}
+
+class MockProductRemoteDataSource extends Mock implements ProductRemoteDataSource {}
+class MockProductLocalDataSource extends Mock implements ProductLocalDataSource {}
+class MockNetworkInfo extends Mock implements NetworkInfo {}
 
 void main() {
-  late ProductRemoteDataSourceImpl dataSource;
-  late MockHttpClient mockHttpClient;
+  late ProductRepositoryImpl repository;
+  late MockProductRemoteDataSource mockRemoteDataSource;
+  late MockProductLocalDataSource mockLocalDataSource;
+  late MockNetworkInfo mockNetworkInfo;
+
+  final testProduct = ProductModel(
+    id: '1',
+    name: 'Test Product',
+    description: 'Description',
+    price: 10.0,
+    imageUrl: 'http://test.com/image.png',
+  );
 
   setUp(() {
-    mockHttpClient = MockHttpClient();
-    dataSource = ProductRemoteDataSourceImpl(client: mockHttpClient);
+    mockRemoteDataSource = MockProductRemoteDataSource();
+    mockLocalDataSource = MockProductLocalDataSource();
+    mockNetworkInfo = MockNetworkInfo();
+
+    repository = ProductRepositoryImpl(
+      remoteDataSource: mockRemoteDataSource,
+      localDataSource: mockLocalDataSource,
+      networkInfo: mockNetworkInfo,
+    );
   });
 
-  final productJson = {
-    "id": "1",
-    "name": "Test Product",
-    "description": "Description",
-    "price": 10,
-    "imageUrl": "http://test.com/image.png"
-  };
+  group('getAllProducts', () {
+    test('should fetch from remote and cache locally when online', () async {
+      when(mockNetworkInfo.isConnected).thenAnswer((_) async => true);
+      when(mockRemoteDataSource.fetchAllProducts()).thenAnswer((_) async => [testProduct]);
+      when(mockLocalDataSource.cacheProduct(testProduct)).thenAnswer((_) async => Future.value());
 
-  final productModel = ProductModel.fromJson(productJson);
-  final productListJson = {
-    "statusCode": 200,
-    "message": "",
-    "data": [productJson]
-  };
+      final result = await repository.getAllProducts();
 
-  group('fetchAllProducts', () {
-    test('returns list of products when response is 200', () async {
-      when(mockHttpClient.get(Uri.parse('${ProductRemoteDataSourceImpl.baseUrl}/products')))
-          .thenAnswer((_) async => http.Response(json.encode(productListJson), 200));
-
-      final result = await dataSource.fetchAllProducts();
-
-      expect(result, isA<List<ProductModel>>());
-      expect(result.length, 1);
-      expect(result.first.id, productModel.id);
+      expect(result, [testProduct]);
+      verify(mockRemoteDataSource.fetchAllProducts()).called(1);
+      verify(mockLocalDataSource.cacheProduct(testProduct)).called(1);
     });
 
-    test('throws exception when response is not 200', () {
-      when(mockHttpClient.get(Uri.parse('${ProductRemoteDataSourceImpl.baseUrl}/products')))
-          .thenAnswer((_) async => http.Response('Not Found', 404));
+    test('should fetch from local when offline', () async {
+      when(mockNetworkInfo.isConnected).thenAnswer((_) async => false);
+      when(mockLocalDataSource.getCachedProducts()).thenAnswer((_) async => [testProduct]);
 
-      expect(dataSource.fetchAllProducts(), throwsException);
+      final result = await repository.getAllProducts();
+
+      expect(result, [testProduct]);
+      verify(mockLocalDataSource.getCachedProducts()).called(1);
+      verifyNever(mockRemoteDataSource.fetchAllProducts());
+    });
+  });
+
+  group('insertProduct', () {
+    test('should insert product when online', () async {
+      when(mockNetworkInfo.isConnected).thenAnswer((_) async => true);
+      when(mockRemoteDataSource.addProduct(testProduct)).thenAnswer((_) async => Future.value());
+      when(mockLocalDataSource.cacheProduct(testProduct)).thenAnswer((_) async => Future.value());
+
+      await repository.insertProduct(testProduct);
+
+      verify(mockRemoteDataSource.addProduct(testProduct)).called(1);
+      verify(mockLocalDataSource.cacheProduct(testProduct)).called(1);
+    });
+
+    test('should throw exception when offline', () async {
+      when(mockNetworkInfo.isConnected).thenAnswer((_) async => false);
+
+      expect(() => repository.insertProduct(testProduct), throwsException);
+
+      verifyNever(mockRemoteDataSource.addProduct(testProduct));
+      verifyNever(mockLocalDataSource.cacheProduct(testProduct));
     });
   });
 
